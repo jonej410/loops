989f3381acd480508dde8a760382bd23dc2e92fa
jonej410 <jonej410@newschool.edu>
2016-02-02 19:09:06 -0500

    Loops (complete)

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -2,16 +2,40 @@
   Classwork
 */
 
-var cellSize = 50;
+var cellSize = 90;
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
 
-  for(var row = 0; row <20;row++){
+  var testCircle = "yellow";
+  var diameter = 100;
 
-    for(var column =0; column <20;column++){
+
+
+  for(var row = 0; row <5;row++){
+
+    for(var column =0; column <5;column++){
+
+fill(random(255),random(255),random(255));
 
       rect(cellSize*column, cellSize*row, cellSize, cellSize);
+
+
+      //for(var slant = 0; slant <5;slant++){
+      //for(var left = 0; slant<5; slant++)
+
+
+  arc(cellSize*column, cellSize*row,90,90,HALF_PI,PI)
+      line(cellSize*column, cellSize*row, cellSize*column+90, cellSize*row+90);
+      fill("mediumPurple")
+      ellipse(cellSize*column, cellSize*row,30,30)
+
+
+
+
+
+
+
 
     }
   }
